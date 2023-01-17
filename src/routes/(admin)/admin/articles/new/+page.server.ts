998f4581ae7a496a960326030c64dc14026f145d
Hunter Johnston <johnstonhuntera@gmail.com>
2023-01-16 21:46:17 -0500

    add createArticle action

--- src/routes/(admin)/admin/articles/new/+page.server.ts
+++ src/routes/(admin)/admin/articles/new/+page.server.ts
@@ -1,9 +1,29 @@
 import { createContext } from "$lib/trpc/context"
 import { router } from "$lib/trpc/router"
 import type { PageServerLoad } from "./$types"
+import type { Actions } from "./$types"
 
 export const load: PageServerLoad = async (event) => {
 	return {
 		tags: router.createCaller(await createContext(event)).tags.list(),
 	}
+}
+
+export const actions: Actions = {
+	createArticle: async (event) => {
+		const body = (await event.request.formData()) as unknown
+
+		try {
+			await router
+				.createCaller(await createContext(event))
+				.articles.create(body)
+		} catch (err) {
+			console.log(err)
+			return fail(400, { message: "Error creating article" })
+		}
+
+		return {
+			success: true,
+		}
+	},
 }
