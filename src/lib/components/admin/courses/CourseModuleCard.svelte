87d4e951127db76775391ec00e761c348cc6c690
Hunter Johnston <johnstonhuntera@gmail.com>
2023-01-21 17:26:02 -0500

    add new lesson button

--- src/lib/components/admin/courses/CourseModuleCard.svelte
+++ src/lib/components/admin/courses/CourseModuleCard.svelte
@@ -3,9 +3,9 @@
 	import Icon from '$lib/components/Icon.svelte'
 </script>
 
-<div class="flex flex-col w-full bg-gray-700 items-center rounded-md">
+<div class="flex flex-col w-full bg-gray-700 items-center rounded-md px-4 pb-2">
 	<!-- Module Item -->
-	<div class="flex w-full items-center justify-between px-4 py-6">
+	<div class="flex w-full items-center justify-between px-2 py-2 h-16">
 		<div class="flex gap-4 items-center">
 			<div>
 				<Icon icon="ph:dots-six-vertical-fill" />
@@ -17,7 +17,7 @@
 		</div>
 	</div>
 	<!-- Lesson Item -->
-	<div class="flex w-full items-center justify-between py-2 pl-10 pr-4">
+	<div class="flex w-full items-center justify-between py-2 pl-8 pr-2 h-16">
 		<div class="flex gap-4 items-center">
 			<div>
 				<Icon icon="ph:dots-six-vertical-fill" />
@@ -28,5 +28,16 @@
 			<Button color="primary" outline size="sm">Edit lesson</Button>
 			<Icon icon="ph:dots-three-outline-vertical" />
 		</div>
+	</div>
+	<!-- New Lesson Button -->
+	<div class="px-2 py-2 h-16 w-full">
+		<Button
+			classes="w-full flex justify-start items-center h-full gap-2 mb-3"
+			color="primary"
+			outline
+		>
+			<Icon icon="ph:plus-circle" />
+			<p>Add new lesson</p>
+		</Button>
 	</div>
 </div>
